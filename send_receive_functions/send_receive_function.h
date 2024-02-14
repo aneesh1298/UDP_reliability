820f7c826430189146326bf7ce6251fb83a75f66
aneesh1298 <144187644+aneesh1298@users.noreply.github.com>
2024-02-13 23:43:04 -0700

    Create send_receive_function.h

---
+++ send_receive_functions/send_receive_function.h
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+
+int sender(int sockfd, char* data, int data_size, int flags,
+           const struct sockaddr *dest_addr, socklen_t addrlen) ;
+int receiver(int sockfd, char* received_data, int max_data_size, int flags,
+             struct sockaddr* src_addr, socklen_t* addrlen) ;
